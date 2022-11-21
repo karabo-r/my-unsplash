d174e90052bad28cca94d6692c6f41f4cc8448ec
Karabo <karaborabothata0@gmail.com>
2022-11-21 00:45:39 -0400

    connect to local batabse

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
+require("dotenv").config()
 const express = require("express")
+const mongoose = require("mongoose")
 
 const app = express()
 const PORT = process.env.PORT || 3001
+
+// connect to mongodb database
+mongoose.connect(process.env.MONGODB_URI_LOCAL)
+.then(()=>console.log("Conntected to database"))
+.catch((error)=>console.log(error))
 
 app.listen(PORT, ()=>console.log("Server running on port", PORT))
